b1651055833e79288ccf86578ee94fe8fb4a6290
YellowWall <89491565+YellowWall@users.noreply.github.com>
2023-10-28 21:42:44 +0000

    stóll kláraður

--- ikea/stoll.js
+++ ikea/stoll.js
@@ -22,7 +22,7 @@ var spinX = 0;
 var spinY = 0;
 var origX;
 var origY;
-
+var zDist = -2.0;
 var fovy = 50.0;
 var near = 0.2;
 var far = 100.0;
@@ -47,7 +47,13 @@ var points = [];
 var normals = [];
 var normalMatrix, normalMatrixLoc;
 var modelViewMatrixLoc;
-var matrixLoc;
+var projectionMatrixLoc;
+var projectionMatrix;
+
+var eye;
+var at = vec3(0.0, 0.0, 0.0);
+var up = vec3(0.0, 1.0, 0.0);
+    
 
 
 window.onload = function init()
@@ -57,41 +63,60 @@ window.onload = function init()
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
-    //colorCube();
-    [points,normals] = normalCube(0.5);
-
     gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+    gl.clearColor( 0.9, 1.0, 1.0, 1.0 );
     
     gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+
+    var dypi = gl.getParameter(gl.DEPTH_BITS);
+    var gildi = gl.getParameter(gl.DEPTH_CLEAR_VALUE);
+    var bil = gl.getParameter(gl.DEPTH_RANGE);
+    //gl.enable(gl.CULL_FACE);
+    //gl.cullFace(gl.BACK);
 
     //
     //  Load shaders and initialize attribute buffers
     //
-    program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
     
+
+    ambientProduct = mult(lightAmbient, materialAmbient);
+    diffuseProduct = mult(lightDiffuse, materialDiffuse);
+    specularProduct = mult(lightSpecular, materialSpecular);
+
+    normalCube();
+
     var nBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, nBuffer);
     gl.bufferData( gl.ARRAY_BUFFER, flatten(normals), gl.STATIC_DRAW );
-
+    
     var vNormal = gl.getAttribLocation( program, "vNormal" );
     gl.vertexAttribPointer( vNormal, 4, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vNormal);
 
     var vBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
-
-    var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
+    
+    var vPosition = gl.getAttribLocation( program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+    
+    modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
+    projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
+    normalMatrixLoc = gl.getUniformLocation( program, "normalMatrix" );
 
     projectionMatrix = perspective( fovy, 1.0, near, far );
-    gl.uniformMatrix4fv( gl.getUniformLocation(program, "projectionMatrix"), false, flatten(projectionMatrix));
+    gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix) );
 
-    normalMatrixLoc = gl.getUniformLocation(program,"normalMatrix");
-    modelViewMatrixLoc = gl.getUniformLocation(program,"modelViewMatrix");
+    
+    gl.uniform4fv( gl.getUniformLocation(program, "ambientProduct"), flatten(ambientProduct) );
+    gl.uniform4fv( gl.getUniformLocation(program, "diffuseProduct"), flatten(diffuseProduct) );
+    gl.uniform4fv( gl.getUniformLocation(program, "specularProduct"), flatten(specularProduct) );	
+    gl.uniform4fv( gl.getUniformLocation(program, "lightPosition"), flatten(lightPosition) );
+    gl.uniform1f( gl.getUniformLocation(program, "shininess"), materialShininess );
     //event listeners for mouse
     canvas.addEventListener("mousedown", function(e){
         movement = true;
@@ -113,82 +138,54 @@ window.onload = function init()
         }
     } );
     numVertices = points.length;
-    makeLight();
     render();
 }
-function makeLight(){
-    ambientProduct = mult(lightAmbient, materialAmbient);
-    diffuseProduct = mult(lightDiffuse, materialDiffuse);
-    specularProduct = mult(lightSpecular, materialSpecular);
-
-    gl.uniform4fv( gl.getUniformLocation(program, "ambientProduct"), flatten(ambientProduct) );
-    gl.uniform4fv( gl.getUniformLocation(program, "diffuseProduct"), flatten(diffuseProduct) );
-    gl.uniform4fv( gl.getUniformLocation(program, "specularProduct"), flatten(specularProduct) );	
-    gl.uniform4fv( gl.getUniformLocation(program, "lightPosition"), flatten(lightPosition) );
-    gl.uniform1f( gl.getUniformLocation(program, "shininess"), materialShininess );
+function normalCube()
+{
+    quad( 1, 0, 3, 2, 0 );
+    quad( 2, 3, 7, 6, 1 );
+    quad( 3, 0, 4, 7, 2 );
+    quad( 6, 5, 1, 2, 3 );
+    quad( 4, 5, 6, 7, 4 );
+    quad( 5, 4, 0, 1, 5 );
 }
-function normalCube(r = 0.5){
-    let npoints = [];
-    let nnormals = [];
-  
-    const indices = [
-      [1, 0, 3, 2],
-      [2, 3, 7, 6],
-      [3, 0, 4, 7],
-      [6, 5, 1, 2],
-      [4, 5, 6, 7],
-      [5, 4, 0, 1],
+function quad(a, b, c, d, n) 
+{
+    var vertices = [
+        vec4( -0.5, -0.5,  0.5, 1.0 ),
+        vec4( -0.5,  0.5,  0.5, 1.0 ),
+        vec4(  0.5,  0.5,  0.5, 1.0 ),
+        vec4(  0.5, -0.5,  0.5, 1.0 ),
+        vec4( -0.5, -0.5, -0.5, 1.0 ),
+        vec4( -0.5,  0.5, -0.5, 1.0 ),
+        vec4(  0.5,  0.5, -0.5, 1.0 ),
+        vec4(  0.5, -0.5, -0.5, 1.0 )
     ];
-    for(var i =0; i< indices.length; ++i) {
-        [a,b,c,d] = indices[i];
-        const [tp,tn] = normQuad(r,a,b,c,d,i);
-        npoints.push(tp);
-        nnormals.push(tn);
 
-    };
-  
-    return [npoints, nnormals];
-  };
-  function normQuad(
-    r,
-    a,
-    b,
-    c,
-    d,
-    n,
-  ){
-    const vertices = [
-      vec4(-r, -r, r, 1.0),
-      vec4(-r, r, r, 1.0),
-      vec4(r, r, r, 1.0),
-      vec4(r, -r, r, 1.0),
-      vec4(-r, -r, -r, 1.0),
-      vec4(-r, r, -r, 1.0),
-      vec4(r, r, -r, 1.0),
-      vec4(r, -r, -r, 1.0),
+    var faceNormals = [
+        vec4( 0.0, 0.0,  1.0, 0.0 ),  // front
+        vec4(  1.0, 0.0, 0.0, 0.0 ),  // right
+        vec4( 0.0, -1.0, 0.0, 0.0 ),  // down
+        vec4( 0.0,  1.0, 0.0, 0.0 ),  // up
+        vec4( 0.0, 0.0, -1.0, 0.0 ),  // back
+        vec4( -1.0, 0.0, 0.0, 0.0 )   // left
     ];
-  
-    const faceNormals = [
-      vec4(0.0, 0.0, 1.0, 0.0), // front
-      vec4(1.0, 0.0, 0.0, 0.0), // right
-      vec4(0.0, -1.0, 0.0, 0.0), // down
-      vec4(0.0, 1.0, 0.0, 0.0), // up
-      vec4(0.0, 0.0, -1.0, 0.0), // back
-      vec4(-1.0, 0.0, 0.0, 0.0), // left
-    ];
-  
-    let indices = [a, b, c, a, c, d];
-  
-    let qpoints = [];
-    let qnormals = [];
-    for (var i = 0; i < indices.length; ++i) {
-      qpoints.push(vertices[indices[i]]);
-      qnormals.push(faceNormals[n]);
+
+    // We need to partition the quad into two triangles in order for
+    // WebGL to be able to render it.  In this case, we create two
+    // triangles from the quad indices
+    
+    //fece normals assigned using the parameter n
+    
+    var indices = [ a, b, c, a, c, d ];
+
+    for ( var i = 0; i < indices.length; ++i ) {
+        points.push( vertices[indices[i]] );
+        normals.push(faceNormals[n]);
+        
     }
-  
-    return [qpoints, qnormals];
-  };
-  
+}
+
 
 const legWidth = 0.1;
 const legHeight = 0.5;
@@ -200,7 +197,7 @@ function render()
 {
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     
-    var mv = mat4();
+    var mv = lookAt(vec3(0.0,0.0,zDist),at,up);
     mv = mult( mv, rotateX(spinX) );
     mv = mult( mv, rotateY(spinY) ) ;
 
@@ -210,7 +207,7 @@ function render()
         vec3(mv[1][0], mv[1][1], mv[1][2]),
         vec3(mv[2][0], mv[2][1], mv[2][2]),
       ];
-  
+    normalMatrix.matrix = true;
     gl.uniformMatrix3fv(normalMatrixLoc, false, flatten(normalMatrix));
 
     // First the right leg front leg
